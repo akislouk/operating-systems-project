6f7ecedf0faf8cd4300c09cd338154aaaf4de0c8
akislouk <spyrosloukatos@gmail.com>
2023-11-29 03:49:55 +0200

    also open 2 TinyOS terminals with -s option

--- build.sh
+++ build.sh
@@ -5,25 +5,28 @@ echo "Build complete"
 
 # Parse command-line options
 while getopts ":s" opt; do
-  case $opt in
-    s)
-      skip=true
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
-  esac
+    case $opt in
+      s)
+        skip=true
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
+      :)
+        echo "Option -$OPTARG requires an argument." >&2
+        exit 1
+        ;;
+    esac
 done
 
 # Skip tests if the -s option is given
 if [ "$skip" = true ]; then
-  echo "Skipping tests"
-  exit 0
+    echo "Skipping tests"
+    gnome-terminal -- bash -c "./terminal 0"
+    gnome-terminal -- bash -c "./terminal 1"
+    ./tinyos_shell 1 2
+    exit 0
 fi
 
 # Run tests
